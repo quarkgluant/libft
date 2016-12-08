d6a37ac08b844a0a2331d9200c126ed85b04ea72
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2016-12-08 11:33:26 +0100

    simplifie streque.c
    en virant les longueurs des chaînes

--- ft_strequ.c
+++ ft_strequ.c
@@ -14,22 +14,12 @@
 
 int			ft_strequ(char const *s1, char const *s2)
 {
-	int		i;
-	int		len_s1;
-	int		len_s2;
-
 	if (s1 && s2)
 	{
-		i = 0;
-		len_s1 = ft_strlen(s1);
-		len_s2 = ft_strlen(s2);
-		if (len_s1 != len_s2)
-			return (0);
-		while (i < len_s1)
+		while (*s1 && *s2)
 		{
 			if (*s1 != *s2)
 				return (0);
-			i++;
 			s1++;
 			s2++;
 		}
